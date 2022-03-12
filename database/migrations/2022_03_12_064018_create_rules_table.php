6188f2d9a20781c771aa7764ea1f317ed5c08ae7
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2022-03-12 11:51:36 +0500

    db

--- database/migrations/2022_03_12_064018_create_rules_table.php
+++ database/migrations/2022_03_12_064018_create_rules_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('rules', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
